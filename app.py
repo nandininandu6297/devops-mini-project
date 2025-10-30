9dd665d86e71a8e0c8d609f02ef97c61dc56e2fb
your-name <your-email>
2025-10-31 02:05:13 +0530

    Your commit message

--- app.py
+++ app.py
@@ -1,24 +1,9 @@
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
+from flask import Flask
+app = Flask(_name_)
 
 @app.route('/')
 def home():
-    return "Welcome to CI/CD Automated Flask App!"
+    return 'Welcome to CI/CD Demo'
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    login_info = {'username': 'admin', 'password': '1234'}
-    message = ""
-    if request.method == 'POST':
-        user = request.form['username']
-        pwd = request.form['password']
-        if user == login_info['username'] and pwd == login_info['password']:
-            message = "Login Successful!"
-        else:
-            message = "Invalid Credentials, Try Again."
-    return render_template('login.html', login_info=login_info, message=message)
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
+if _name_ == '_main_':
+    app.run(host='0.0.0.0', port=5000)
